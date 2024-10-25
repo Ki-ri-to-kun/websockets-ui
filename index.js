5075bfcc1f3e7e16797b728367d7fd46548d80a5
stas <staskazakov9@gmail.com>
2024-10-25 10:48:26 +0600

    start: websoket connection done

--- index.js
+++ index.js
@@ -1,6 +1,56 @@
-import { httpServer } from "./src/http_server/index.js";
+import {WebSocketServer} from 'ws';
+import {v4 as uuid} from 'uuid';
 
-const HTTP_PORT = 8181;
+import { httpServer } from './src/http_server/index.js';
+import {users} from './src/data/users.js';
+import {messageType} from './src/messages/constants.js';
+
+
+const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wsServer = new WebSocketServer({noServer: true});
+
+wsServer.on('connection', (ws) => {
+  ws.on('message', message => {
+    const messageText = message.toString();
+    const messageObj = JSON.parse(messageText);
+    
+    // checking message type
+    switch(messageObj.type){
+      case messageType.REG:
+        const data = {
+                name: 'username',
+                index: '00001',
+                error: false,
+                errorText: ''
+        };
+        
+        const dataJson = JSON.stringify(data);
+        
+         const response =  {
+          type: 'reg',
+          data: dataJson,
+          id: 0
+        };
+        
+        ws.send(JSON.stringify(response));
+        break;
+      default: 
+        console.log('unknown command');
+        console.log(messageObj.type);
+    }
+    
+   
+  });
+  ws.send(JSON.stringify('websocket connected!'));
+});
+
+httpServer.on('upgrade', (req, socket, head) => {
+
+  wsServer.handleUpgrade(req, socket, head, (ws) => {
+    wsServer.emit('connection', ws, req);
+  });
+});
---
+++ src/messages/constants.js
@@ -0,0 +1,10 @@
+export const messageType = {
+  REG: 'reg',
+  CREATE_GAME:'create_game',
+  START_GAME: 'start_game',
+  TURN: 'turn',
+  ATTACK: 'attack',
+  FINISH: 'finish',
+  UPDATE_ROOM: 'update_room',
+  UPDATE_WINNERS: 'update_winners'
+};
